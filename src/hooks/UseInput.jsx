b857a78fa4cb1d38cbac2ca0d8b5de3bfb2a4b6e
Parimal Najan <63017672+parimalnajan@users.noreply.github.com>
2022-12-09 03:48:48 +0530

    adds useInput hook to easily add and manage Input fields

--- src/hooks/UseInput.jsx
+++ src/hooks/UseInput.jsx
@@ -1,5 +1,13 @@
-function useInput({ type, className /*...*/ }) {
-    const [value, setValue] = useState("");
-    const input = <input className={className} value={value} onChange={e => setValue(e.target.value)} type={type} />;
-    return [value, input];
+import React , { useCallback, useState } from "react";
+
+export function useInput( type, styles) {
+    const [value, setValue] = useState('');
+    const clearValue = useCallback(
+      () => {
+         setValue("")
+      },
+      [],
+    )
+    const input = <input type={type} className={`${styles} m-8 `} value={value} onChange={e => setValue(e.target.value)}  />;
+    return [value,clearValue, input];
   }
