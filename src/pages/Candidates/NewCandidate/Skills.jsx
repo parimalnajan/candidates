5441ce0d0a9536de9af7fc45212c2ad2fe3fcf6c
Parimal Najan <63017672+parimalnajan@users.noreply.github.com>
2022-12-09 03:49:33 +0530

    adds skills section, moves form submit button to this section temporarily

--- src/pages/Candidates/NewCandidate/Skills.jsx
+++ src/pages/Candidates/NewCandidate/Skills.jsx
@@ -1,8 +1,64 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { useEffect } from 'react'
+import { FormContext } from '.'
+import { useInput } from '../../../hooks/UseInput'
 
 const Skills = () => {
+  const {steps,currentStep,currentCandidate,setFormState,formState} = useContext(FormContext)
+
+  const [skill,clearSkill,skillJSX]=useInput("text","p-4 ")
+  const [experience,clearExperience,experienceJSX]=useInput("number","p-8")
+
+  const [skillsArray,setSkillsArray] = useState(currentCandidate.skills)
+  useEffect(() => {
+    console.log(skillsArray,currentCandidate);
+  
+  
+  }, [skillsArray])
+  
+
+  const submitSkills = () =>{
+    setFormState((oldState)=>{console.log(oldState);return {...oldState,skills:skillsArray}})
+  }
+
   return (
-    <div>Skills</div>
+    <div>
+      <h4 className='text-lg font-semibold text-gray-600 my-4 mb-1'>Skills Summary</h4>
+
+        {skillsArray.length===0?<> Your skills will appear here</>:<></>}
+      <div className='mb-8 flex gap-2'>
+        {
+              skillsArray?.map((skill,index)=>{
+                return (
+                  <div className='px-2 py-1 rounded-lg bg-gray-200 w-fit'>
+                    {skill.name} ({skill.experience})
+                     <button onClick={()=>setSkillsArray(old=>
+                      old.filter((data)=> data!= skill))}
+                      className='pl-1 ml-1 text-red-600 '>
+                      x
+                      </button>
+                  </div>)
+              })
+            }
+          </div>
+        
+       <legend>Skill</legend>
+        <div>  {skillJSX}</div>
+       <legend>Experience (months)</legend>
+
+        <div>{experienceJSX}</div>
+
+        <button onClick={(e)=>{ e.preventDefault();
+        if(!skillsArray)
+          setSkillsArray([{name:skill,experience:experience}])
+        else{setSkillsArray(skillsArray=>[...skillsArray,{name:skill,experience:experience}]);} 
+       }}
+        className='bg-purple-400 px-2 py-1 rounded-md'>Add</button>
+
+      <button onClick={submitSkills} className='button-base block mt-8 bg-blue-400'>Save and proceed</button>
+
+
+    </div>
   )
 }
 
